51ffbfeedd2eb5416f80d9f95b7ec2f060f34442
Jakub Bednarz <kubabednarz1709@gmail.com>
2025-05-23 20:02:43 +0200

    road port chances reward

--- envs/init_placement_env/step_mixins.py
+++ envs/init_placement_env/step_mixins.py
@@ -90,18 +90,25 @@ class CatanStepMixin:
             self._obs["edges"]["is_built"][node, adjacent_nodes.index(node)] = 1
             # Set ownership maybe baby
 
+    def __get_other_adjacent_nodes(self, node, known_node):
+        possible_nodes = NODES_TO_NODES[node]
+        possible_nodes.remove(known_node)
+        return possible_nodes
+
+    """
+    Road reward function
+    """
     def __evaluate_road_heuristic(self, road_action, node_index):
         road_index = np.argmax(road_action)
         road_nodes = EDGES_LIST[road_index]
         target_node = road_nodes[0] if road_nodes[1] == node_index else road_nodes[1]
-        possible_nodes = NODES_TO_NODES[target_node]
-        possible_nodes.remove(node_index)
+        possible_nodes = self.__get_other_adjacent_nodes(target_node, node_index)
 
         # Heuristic 1: Estimation of two possible settlements road can lead to (0 if already occupied or impossible)
         potential_value = self.__estimate_future_node_values(possible_nodes)
 
         # Heuristic 2: Is it directed towards a port?
-        is_toward_port = self.__check_if_toward_port(possible_nodes)
+        is_toward_port = self.__check_if_toward_port(target_node, possible_nodes)
 
         return potential_value + is_toward_port * 0.3
 
@@ -118,5 +125,37 @@ class CatanStepMixin:
             value += np.mean(norm_prob)
         return value
 
-    def __check_if_toward_port(self, possible_nodes):
-        return 0
+    def __has_port(self, node):
+        return self._obs["has_port"][node].sum() > 0
+
+    """
+    Returns points for port chances
+    1 - for close port (one more edge)
+    0.4 - for far port (two more edges)
+    0 - no found port chances
+    Assumption: ports are of equal value at the beginning
+    """
+    def __check_if_toward_port(self, base_node, possible_nodes):
+        value = 0
+        for node in possible_nodes:
+            # Check if road can be placed
+            edge_index = EDGES_LIST.index((min(base_node, node), max(base_node, node)))
+            if not self.__is_valid_road_placement(edge_index):
+                continue
+            # Check for possible port
+            if self.__has_port(node) and self.__is_valid_settlement_placement(node):
+                # Close port - full point
+                value += 1
+                break
+            # Check further port
+            further_nodes = self.__get_other_adjacent_nodes(node, base_node)
+            for further_node in further_nodes:
+                edge_index = EDGES_LIST.index((min(node, further_node), max(node, further_node)))
+                if not self.__is_valid_road_placement(edge_index):
+                    continue
+                # Check for possible port
+                if self.__has_port(node) and self.__is_valid_settlement_placement(node):
+                    # Far port - lower value
+                    value += 0.4
+                    break
+        return value
