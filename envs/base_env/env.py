ceee053d4fd8681f63854e5fd7c4fdcc428752a3
Procentaurus <michalski.ob@gmail.com>
2025-05-28 20:56:28 +0200

    [ENV] Fixed base env reset() mechanics

--- envs/base_env/env.py
+++ envs/base_env/env.py
@@ -34,21 +34,22 @@ class CatanBaseEnv(gym.Env):
 
     def reset(self):
         resources = self.__generate_resources()
-        tokens = self.__generate_tokens(resources)
-        robber_index = np.argmax(resources[:, -1])  # Desert tile index
+        desert_tile_id = self.__get_desert_tile_id(resources)
+        tokens = self.__generate_tokens(desert_tile_id)
+        robber_index = desert_tile_id
 
         self.state = {
             "tiles": {
                 "resources": resources,
                 "tokens": tokens,
-                "has_robber": np.eye(self.N_TILES)[robber_index],
+                "has_robber": np.eye(N_TILES)[robber_index],
                 "nodes": {
-                    "is_settlement": np.zeros((self.N_TILES, 6),
+                    "is_settlement": np.zeros((N_TILES, 6),
                                               dtype=np.int8),
-                    "is_city": np.zeros((self.N_TILES, 6),
+                    "is_city": np.zeros((N_TILES, 6),
                                         dtype=np.int8),
-                    "owner": np.zeros((self.N_TILES, 6, N_PLAYERS),
-                                        dtype=np.int8),
+                    "owner": np.zeros((N_TILES, 6, N_PLAYERS),
+                                      dtype=np.int8),
                     "has_port": np.zeros((N_TILES, 6, N_PORT_FIELD_TYPES),
                                          dtype=np.int8),
                     "ports": self.__generate_ports()
@@ -66,40 +67,40 @@ class CatanBaseEnv(gym.Env):
         Return a MultiBinary matrix of shape (19, 6), one-hot per tile
         """
         res_list = (
-            [0] * TILE_TYPE_COUNTS["brick"] +
-            [1] * TILE_TYPE_COUNTS["wood"] +
-            [2] * TILE_TYPE_COUNTS["wool"] +
-            [3] * TILE_TYPE_COUNTS["grain"] +
-            [4] * TILE_TYPE_COUNTS["ore"] +
-            [5] * TILE_TYPE_COUNTS["desert"]
+            [0] * TILE_TYPE_COUNTS[RESOURCE_TYPES[0]] +
+            [1] * TILE_TYPE_COUNTS[RESOURCE_TYPES[1]] +
+            [2] * TILE_TYPE_COUNTS[RESOURCE_TYPES[2]] +
+            [3] * TILE_TYPE_COUNTS[RESOURCE_TYPES[3]] +
+            [4] * TILE_TYPE_COUNTS[RESOURCE_TYPES[4]] +
+            [5] * TILE_TYPE_COUNTS[RESOURCE_TYPES[5]]
         )
         np.random.shuffle(res_list)
-        return np.eye(N_RESOURCE_TYPES)[res_list]
+        return np.eye(N_RESOURCE_TYPES)[res_list].astype(np.int8)
 
-    def __generate_tokens(self):
-        token_values = ALL_TOKENS
-        np.random.shuffle(token_values)
-        tokens = np.zeros((N_TILES, N_TOKEN_VALUES), dtype=np.int32)
+    def __generate_tokens(self, desert_tile_id):
+        tokens = np.zeros((N_TILES, N_TOKEN_VALUES), dtype=np.int8)
+
+        shuffled_tokens = ALL_TOKENS.copy()
+        np.random.shuffle(shuffled_tokens)
 
         token_idx = 0
-        desert_tile_index = self.__get_desert_tile_index()
         for i in range(N_TILES):
-            if i == desert_tile_index:
+            if i == desert_tile_id:
                 continue
-            token_val = token_values[token_idx]
-            tokens[i][token_val - 2] = 1  # map 2-12 to index 0-10
+            token_val = shuffled_tokens[token_idx]
+            tokens[i][token_val - 2] = 1  # Map token 2–12 to index 0–10
             token_idx += 1
         return tokens
 
     def __generate_ports(self):
         res_list = (
-            [0] * PORT_TYPE_COUNTS["brick"] +
-            [1] * PORT_TYPE_COUNTS["wood"] +
-            [2] * PORT_TYPE_COUNTS["wool"] +
-            [3] * PORT_TYPE_COUNTS["grain"] +
-            [4] * PORT_TYPE_COUNTS["ore"] +
-            [5] * PORT_TYPE_COUNTS["generic"] +
-            [6] * PORT_TYPE_COUNTS["no_port"]
+            [0] * PORT_TYPE_COUNTS[PORT_TYPES[0]] +
+            [1] * PORT_TYPE_COUNTS[PORT_TYPES[1]] +
+            [2] * PORT_TYPE_COUNTS[PORT_TYPES[2]] +
+            [3] * PORT_TYPE_COUNTS[PORT_TYPES[3]] +
+            [4] * PORT_TYPE_COUNTS[PORT_TYPES[4]] +
+            [5] * PORT_TYPE_COUNTS[PORT_TYPES[5]] +
+            [6] * PORT_TYPE_COUNTS[PORT_TYPES[6]]
         )
         np.random.shuffle(res_list)
         node_ports = [val for val in res_list for _ in range(2)]
@@ -112,12 +113,11 @@ class CatanBaseEnv(gym.Env):
                 border_index += 1
 
         tile_ports = np.zeros((N_TILES, 6, N_PORT_FIELD_TYPES), dtype=np.int8)
-        for tile_id, node_ids in enumerate(TILES_TO_NODES):
+        for tile_id, node_ids in TILES_TO_NODES.items():
             for i, node_id in enumerate(node_ids):
                 if len(NODES_TO_TILES[node]) <= 2:
                     tile_ports[tile_id, i] = result[node_id]
         return tile_ports
 
-    def __get_desert_tile_index(self):
-        resources = self.state["tiles"]["resources"]
+    def __get_desert_tile_id(self, resources):
         return np.argmax(resources[:, -1])
