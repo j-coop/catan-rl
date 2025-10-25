41f0c38c1e90e23e03dfc2b093f29235ba71b664
Jakub Bednarz <kubabednarz1709@gmail.com>
2025-10-25 21:59:15 +0200

    circular import and pyqt fixes

--- marl/ui/main.py
+++ marl/ui/main.py
@@ -1,6 +1,9 @@
 # main.py
 from marl.ui.GameController import GameController
 
+import os
+os.environ["QT_QPA_PLATFORM"] = "xcb"
+
 if __name__ == "__main__":
     controller = GameController()
     controller.start()
