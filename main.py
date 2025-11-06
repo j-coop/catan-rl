bec8f44dba32b1777cd329e5dd61847f68951002
Procentaurus <michalski.ob@gmail.com>
2025-11-06 15:08:42 +0100

    [PLOT] Fixed plotting issue

--- main.py
+++
@@ -1,8 +0,0 @@
-from envs.base_env.env import CatanBaseEnv
-from visualization.map_plotter import CatanMapPlotter
-
-env = CatanBaseEnv()
-obs = env.reset()
-
-plotter = CatanMapPlotter(obs)
-plotter.plot_catan_map()
